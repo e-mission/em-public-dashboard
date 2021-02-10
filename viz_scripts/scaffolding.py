9ed8041683ccde29a4a28b3729715dc1daba0515
Shankari <shankari@eecs.berkeley.edu>
2021-02-09 22:38:06 -0800

    Make it easier to create other visualizations
    - Pull the code to read, filter and expand the confirmed_trips into a separate module
    - Modify the existing notebook to call the module; this leads to a very simplified implementation
    - Check in both the notebook and the module
    
    Should we move other functions (maybe the `quality_text` generation) into the module?

---
+++ viz_scripts/scaffolding.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+import emission.storage.timeseries.abstract_timeseries as esta
+import emission.storage.timeseries.tcquery as esttc
+import emission.core.wrapper.localdate as ecwl
+
+# Module for pretty-printing outputs (e.g. head) to help users
+# understand what is going on
+# However, this means that this module can only be used in an ipython notebook
+import IPython.display as disp
+
+import emission.core.get_database as edb
+
+def get_time_query(year, month):
+    query_ld = ecwl.LocalDate({"year": year, "month": month})
+    tq = esttc.TimeComponentQuery("data.start_local_dt", query_ld, query_ld)
+    return tq
+
+def get_participant_uuids(program):
+    """
+        Get the list of participant UUIDs for the specified program.
+        Note that the "program" parameter is currently a NOP but will be enabled
+        once we have other programs start
+    """
+    participant_uuid_obj = list(edb.get_profile_db().find({"install_group": "participant"}, {"user_id": 1, "_id": 0}))
+    participant_uuid_str = [u["user_id"] for u in participant_uuid_obj]
+    disp.display(participant_uuid_str)
+    return participant_uuid_str
+
+def load_all_confirmed_trips(tq):
+    agg = esta.TimeSeries.get_aggregate_time_series()
+    all_ct = agg.get_data_df("analysis/confirmed_trip", tq)
+    print("Loaded all confirmed trips of length %s" % len(all_ct))
+    disp.display(all_ct.head())
+    return all_ct
+
+def load_all_participant_trips(program, tq):
+    participant_list = get_participant_uuids(program)
+    all_ct = load_all_confirmed_trips(tq)
+    participant_ct_df = all_ct[all_ct.user_id.isin(participant_list)]
+    print("After filtering, found %s participant trips " % len(participant_ct_df))
+    disp.display(participant_ct_df.head())
+    return participant_ct_df
+
+def filter_labeled_trips(mixed_trip_df):
+    labeled_ct = mixed_trip_df[mixed_trip_df.user_input != {}]
+    print("After filtering, found %s labeled trips" % len(labeled_ct))
+    disp.display(labeled_ct.head())
+    return labeled_ct
+
+def expand_userinputs(labeled_ct):
+    label_only = pd.DataFrame(labeled_ct.user_input.to_list(), index=labeled_ct.index)
+    disp.display(label_only.head())
+    expanded_ct = pd.concat([labeled_ct, label_only], axis=1)
+    assert len(expanded_ct) == len(labeled_ct), \
+        ("Mismatch after expanding labels, expanded_ct.rows = %s != labeled_ct.rows" %
+            (len(expanded_ct), len(labeled_ct)))
+    print("After expanding, columns went from %s -> %s" %
+        (len(labeled_ct.columns), len(expanded_ct.columns)))
+    assert len(expanded_ct.columns) == len(labeled_ct.columns) + 3, \
+        ("Mismatch after expanding labels, expanded_ct.columns = %s != labeled_ct.rows" %
+            (len(expanded_ct.columns), len(labeled_ct.columns)))
+    disp.display(expanded_ct.head())
+    return expanded_ct
