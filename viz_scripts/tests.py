840ff24d899862f9df8d36aeb6689b166ddda4d6
Young, Stanley A <Stanley.A.Young@nrel.gov>
2022-01-13 11:42:43 -0700

    Added Cost and Time Calculations by Mode

---
+++ viz_scripts/tests.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import scaffolding
+
+def test_energy_intensity():
+
+    # Inputs
+    dummy_data = pd.DataFrame({
+
+    })
+
+    
+
+
+
+
+
+def test_energy_impact_kWH():
+    #
+    None
