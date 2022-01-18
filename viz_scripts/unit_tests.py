55cd7cd815c0358c2bd9a2cad7fe28df1dce936b
Young, Stanley A <Stanley.A.Young@nrel.gov>
2022-01-18 09:24:41 -0700

    Unit Test Energy Impact

--- viz_scripts/unit_tests.py
+++ viz_scripts/unit_tests.py
@@ -14,6 +14,10 @@ import numpy as np
 import scaffolding
 
 class TestEnergyIntensity(unittest.TestCase):
+    """
+    A unit test for energy_intensity function in 
+    the scaffolding.py file
+    """
     
     def setUp(self):
         self.constants = pd.DataFrame({
@@ -24,7 +28,7 @@ class TestEnergyIntensity(unittest.TestCase):
             'CO2_factor': [1, 2, 3],
             '(kWH)/trip': [0.5, 0.2, 0.3]
         })
-        # Inputs
+
         self.data = pd.DataFrame({
             'mode': ['car', 'bus', 'train', 'car'],
             'repm': ['car', 'car', 'bus', 'train'],
@@ -77,7 +81,75 @@ class TestEnergyIntensity(unittest.TestCase):
                             f'{output}')
 
 
+class TestEnergyImpact(unittest.TestCase):
+    """
+    A unit test for energy_impact_kWH function in 
+    the scaffolding.py file
+    """
 
+    def setUp(self):
+        self.conditions = np.array([
+            [True, False, False],
+            [False,True,False],
+            [False,False,True]
+        ])
+        self.values = np.array([
+            [8, 0, 3],
+            [3,5,7],
+            [4,2,9]
+        ])
+        self.data = pd.DataFrame({
+            'mode': ['car', 'bus', 'train', 'car'],
+            'repm': ['car', 'car', 'bus', 'train'],
+            'dist': [1.5,2.5,3.5,4.5],
+            'ei_mode': [1,2,3,1],
+            'ei_repm': [1,1,2,3],
+            'ei_trip_mode': [7,8,9,7],
+            'ei_trip_repm': [7,7,8,9],
+            'Mode_confirm_fuel': ['gasoline','diesel','electric','gasoline'],
+            'Replaced_mode_fuel': ['gasoline','gasoline','diesel','electric']
+        })
+
+
+    def test_process(self):
+        expect = np.array([8, 5, 9])
+        output = np.select(self.conditions, self.values)
+        if(len(expect) != len(output)):
+            self.assertTrue(False, 
+                            f'Select Malfunction (out: {output})')
+        else:
+            for i in range(len(expect)):
+                self.assertEqual(expect[i], output[i],
+                                f'Select Malfunction (out: {output})')
+    
+    def test_function(self):
+        expect = pd.DataFrame({
+            'mode': ['car', 'bus', 'train', 'car'],
+            'repm': ['car', 'car', 'bus', 'train'],
+            'dist': [1.5,2.5,3.5,4.5],
+            'ei_mode': [1,2,3,1],
+            'ei_repm': [1,1,2,3],
+            'ei_trip_mode': [7,8,9,7],
+            'ei_trip_repm': [7,7,8,9],
+            'Mode_confirm_fuel': ['gasoline','diesel','electric','gasoline'],
+            'Replaced_mode_fuel': ['gasoline','gasoline','diesel','electric'],
+            'repm_EI(kWH)':[1.5*1*0.000293071,
+                            2.5*1*0.000293071,
+                            3.5*2*0.000293071,
+                            4.5*3+9],
+            'mode_EI(kWH)':[1.5*1*0.000293071,
+                            2.5*2*0.000293071,
+                            3.5*3+9,
+                            4.5*1*0.000293071],
+            'Energy_Impact(kWH)':[round(1.5*1*0.000293071-1.5*1*0.000293071,3), 
+                                  round(2.5*1*0.000293071-2.5*2*0.000293071,3),
+                                  round(3.5*2*0.000293071-(3.5*3+9),3), 
+                                  round(4.5*3+9-4.5*1*0.000293071,3)]
+        })
+        output = scaffolding.energy_impact_kWH(self.data,'dist','repm', 'mode')
+        self.assertTrue(np.isclose(expect['Energy_Impact(kWH)'],
+                                   output['Energy_Impact(kWH)']).all(),
+                            f'Error in function')
 
 if __name__ == '__main__':
     unittest.main()
