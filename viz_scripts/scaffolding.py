89be96229f1c42e6b66b80e5191e87dfd30c8759
zackAemmer <zae5op@gmail.com>
2022-06-29 12:38:53 -0700

    Small typo (again)

--- viz_scripts/scaffolding.py
+++ viz_scripts/scaffolding.py
@@ -65,7 +65,7 @@ def expand_userinputs(labeled_ct):
     assert len(expanded_ct) == len(labeled_ct), \
         ("Mismatch after expanding labels, expanded_ct.rows = %s != labeled_ct.rows" %
             (len(expanded_ct), len(labeled_ct)))
-    print("After expanding, columns went from %s -> %s" %s
+    print("After expanding, columns went from %s -> %s" %
         (len(labeled_ct.columns), len(expanded_ct.columns)))
     assert len(expanded_ct.columns) == len(labeled_ct.columns) + 3, \
         ("Mismatch after expanding labels, expanded_ct.columns = %s != labeled_ct.columns" %
