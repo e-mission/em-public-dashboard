f8febf2b0edbe598555d63cb11e905a12d7c1ce7
zackAemmer <zae5op@gmail.com>
2022-08-25 10:55:33 -0700

    Load config from Github and insert params into notebooks

--- viz_scripts/bin/generate_plots.py
+++ viz_scripts/bin/generate_plots.py
@@ -3,6 +3,13 @@ import nbformat
 import nbparameterise as nbp
 import argparse
 import arrow
+import json
+import requests
+
+
+# Configuration settings to use for all generated plots by this instance
+# This could also be specified as a parser argument, if we want to generate plots for all programs from one instance
+STUDY_CONFIG = "nrel-commute"
 
 parser = argparse.ArgumentParser(prog="generate_metrics")
 parser.add_argument("plot_notebook", help="the notebook the generates the plot")
@@ -18,6 +25,16 @@ if args.date is None:
     yesterday = arrow.get()
     args.date = [yesterday.year, yesterday.month]
 
+# Read and use parameters from the unified config file on the e-mission Github page
+r = requests.get("https://raw.githubusercontent.com/e-mission/nrel-openpath-deploy-configs/main/configs/" + STUDY_CONFIG + ".nrel-op.json")
+if r.status_code is not 200:
+    print(f"Unable to download study config, status code: {r.status_code}")
+else:
+    dynamic_config = json.loads(r.text)
+    print(f"Successfully downloaded config with version {dynamic_config['version']} "\
+        f"for {dynamic_config['intro']['translated_text']['en']['deployment_name']} "\
+        f"and data collection URL {dynamic_config['server']['connectUrl']}")
+
 print(f"Running at {arrow.get()} with args {args}")
 
 with open(args.plot_notebook) as f:
@@ -27,8 +44,12 @@ with open(args.plot_notebook) as f:
 orig_parameters = nbp.extract_parameters(nb)
 
 # Update one or more parameters
-params = nbp.parameter_values(orig_parameters,
-    year=args.date[0], month=args.date[1], program=args.program)
+params = nbp.parameter_values(
+    orig_parameters,
+    year=args.date[0],
+    month=args.date[1],
+    program=args.program,
+    study_type=dynamic_config['intro']['program_or_study'])
 
 # Make a notebook object with these definitions
 new_nb = nbp.replace_definitions(nb, params, execute=False)
